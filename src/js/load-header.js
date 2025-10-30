5c736a46c83985f3beb7439e8834c3b7f592e233
JumpStone <143991347+Jumpstone@users.noreply.github.com>
2025-10-30 23:12:35 +0100

    Changes

--- src/js/load-header.js
+++ src/js/load-header.js
@@ -1,31 +1,39 @@
 window.addEventListener("DOMContentLoaded", () => {
-    fetch("https://cdn.grueneeule.de/templates/header/grueneeule-web/header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-container").innerHTML = data;
-        });
+  fetch(
+    "https://cdn.grueneeule.de/templates/header/de/grueneeule-web/header.html"
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-de-container").innerHTML = data;
+    });
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    fetch("https://cdn.grueneeule.de/templates/header/grueneeule-web/header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-en-container").innerHTML = data;
-        });
+  fetch(
+    "https://cdn.grueneeule.de/templates/header/en/grueneeule-web/header.html"
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-en-container").innerHTML = data;
+    });
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    fetch("https://cdn.grueneeule.de/templates/header/grueneeule-web/header-dev.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-dev-container").innerHTML = data;
-        });
+  fetch(
+    "https://cdn.grueneeule.de/templates/header/de/grueneeule-web/header-dev.html"
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-dev-de-container").innerHTML = data;
+    });
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    fetch("https://cdn.grueneeule.de/templates/header/toollab-web/header.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("header-toollab-container").innerHTML = data;
-        });
+  fetch(
+    "https://cdn.grueneeule.de/templates/header/en/grueneeule-web/header-dev.html"
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-dev-en-container").innerHTML = data;
+    });
 });
