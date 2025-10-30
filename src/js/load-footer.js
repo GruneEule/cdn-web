228198961d69b751b31cd3f30810777c3a10b8f1
JumpStone <143991347+Jumpstone@users.noreply.github.com>
2025-10-30 23:19:30 +0100

    Changes

--- src/js/load-footer.js
+++ src/js/load-footer.js
@@ -1,15 +1,19 @@
 window.addEventListener("DOMContentLoaded", () => {
-    fetch("https://grueneeule.de/assets/templates/footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-container").innerHTML = data;
-        });
+  fetch(
+    "https://cdn.grueneeule.de/templates/footer/grueneeule-web/de/footer.html"
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer-de-container").innerHTML = data;
+    });
 });
 
 window.addEventListener("DOMContentLoaded", () => {
-    fetch("https://grueneeule.de/assets/templates/en/footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-en-container").innerHTML = data;
-        });
+  fetch(
+    "https://cdn.grueneeule.de/templates/footer/grueneeule-web/en/footer.html"
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer-en-container").innerHTML = data;
+    });
 });
